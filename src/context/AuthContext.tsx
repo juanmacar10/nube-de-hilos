5c9d92b706d147bb899824633ae763060c183f9c
juanmacar10 <juandamadrid15@gmail.com>
2026-04-29 18:12:03 -0500

    add context and firebase config

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/config';
+import { onAuthStateChanged, type User } from 'firebase/auth';
+
+interface AuthContextType {
+    user: User | null;
+    loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType>({ user: null, loading: true });
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false);
+        });
+        return unsubscribe;
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext);
